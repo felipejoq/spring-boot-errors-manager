476ed0c7c7d202cb6e88e4ca56c2c67573fca2cd
felipejoq <felipejofreq@gmail.com>
2022-12-18 11:14:14 -0300

    Manejo de errores por convención usando nombres en las visas dentro de la carpeta error/codigoerror.html

---
+++ src/main/java/com/uncodigo/springboot/error/app/controllers/AppController.java
@@ -0,0 +1,18 @@
+package com.uncodigo.springboot.error.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		
+		Integer valor = 100/0;
+		
+		return "index";
+	}
+
+}
